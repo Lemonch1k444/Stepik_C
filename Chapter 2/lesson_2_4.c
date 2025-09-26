04c0b4fc0b85289175a74b62c1c59c65003b563b
Lemonch1k <Lemonch1k444@gmail.com>
2025-09-26 12:04:39 +0300

    2.4

---
+++ Chapter 2/lesson_2_4.c
@@ -0,0 +1,8 @@
+#include <stdio.h>
+//%d or %i - целое число со знаком в десятичной форме, %u - десятичная форма, %o - восьмеричная форма, %x - шестнадцатеричная форма, %c - character, %s - string
+int main(void)
+{
+    int var_i = 1208;
+    printf("value = \n");
+    return 0;
+}
