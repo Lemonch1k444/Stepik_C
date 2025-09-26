9a7a1eca26b3d63b2e163e17b848c7d27de1d6b3
Lemonch1k <Lemonch1k444@gmail.com>
2025-09-26 23:10:20 +0300

    lesson 2.4

--- Chapter 2/lesson_2_4.c
+++ Chapter 2/lesson_2_4.c
@@ -2,7 +2,21 @@
 //%d or %i - целое число со знаком в десятичной форме, %u - десятичная форма, %o - восьмеричная форма, %x - шестнадцатеричная форма, %c - character, %s - string
 int main(void)
 {
-    int var_i = 1208;
-    printf("value = \n");
+    /*long long var_i = -12345678901234;
+    long double var_ld = 5456767.567951;
+    // char, short -> int in printf
+    // float -> double in printf
+    // %lld for long long, %ld
+     L - суффикс для long double! 
+    printf("value = %Lf\n", var_ld); */
+
+    short var_h = -100;
+    int var_i = 1024;
+    long double var_ld = 0.5;
+
+    printf("var_h = %d, var_i = %d, %Lf\n", var_h, var_i, var_ld);
+
+    printf("[%+10d]\n", var_i); //10 символов внутри скобочек
+    printf("[%-10.4d]\n", var_h); //10 символов, 4 символа длина числа
     return 0;
 }
